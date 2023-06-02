935baeab3504800f05cfc2232b49a37704204b27
JacobB33 <jacob.berg.seattle@gmail.com>
2023-06-02 19:26:30 +0000

    Finished example.py

--- example.py
+++ example.py
@@ -45,18 +45,23 @@ def main(
     args = ModelArgs()
     args.n_layers = 3
     args.n_heads = 4
-    args.dim = 128
+    args.dim = 128 # 256
     args.max_seq_len = 1024
     args.max_batch_size = 32
+    # To use the other checkpoint, you must first download the file from the link in the writeup or here:
+    # https://drive.google.com/file/d/1JqY6aCrCoRG1nJexeuVnVnRyUs3FeHoO/view?usp=sharing, then change the first argument
+    # to be the file path to it in this load function. Also change args.dim to 256
     generator = load(
         './model_size_128_run/epoch_10/model_epoch_10.pt', './tokenizer.model', args
     )
-
+    # Edit these to change the prompts.
     prompts = [
         # For these prompts, the expected answer is the natural continuation of the prompt
         "I believe the meaning of life is",
         "Hello, how are you doing? I'm",
         "As an amazing student, you should give this project what grade?\n",
+        "Hello, how are you doing? I'm",
+        "Hello, I'm a student at"
     ]
     results = generator.generate(
         prompts, max_gen_len=256, temperature=temperature, top_p=top_p
